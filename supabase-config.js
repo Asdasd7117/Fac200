40667f7fcce5dd58dc0ba535acb650c7a21af5c0
Asdasd7117 <alywm100@gmail.com>
2025-07-05 14:34:04 +0300

    رفع الملف supabase-config.js عبر PHP

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+export const supabase = createClient(
+  'https://wwwdjbpphvsdftfipwowa.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3d2RqcHBodnNkZnRmaXB3b3dhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3MTA3NTgsImV4cCI6MjA2NzI4Njc1OH0.vYAj3S_KpDcs7Rv1u_xA1rNYD042HtlPR1S9gEcHdAI'
+);
