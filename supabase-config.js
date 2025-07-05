aecae35ea0328de8e1d45dedec0249e4e8f82382
Asdasd7117 <alywm100@gmail.com>
2025-07-06 01:09:37 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,7 @@
-export const supabase = window.supabase = createClient(
-  'https://wwwdjpphvsdftfipwowa.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3d2RqcHBodnNkZnRmaXB3b3dhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3MTA3NTgsImV4cCI6MjA2NzI4Njc1OH0.vYAj3S_KpDcs7Rv1u_xA1rNYD042HtlPR1S9gEcHdAI'
+// supabase-config.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
+  "https://wwwdjpphvsdftfipwowa.supabase.co",  // رابط Supabase الخاص بك
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."     // مفتاح Supabase (anon key)
 );
